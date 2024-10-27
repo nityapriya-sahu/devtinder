9db3d0cbe2c946fe0243e12d5fc3161ab364687c
nityapriya-sahu <sahunityapriya@gmail.com>
2024-10-27 15:20:46 +0530

    explore get, post, delete, put method

--- src/app.js
+++ src/app.js
@@ -2,16 +2,43 @@ const express = require("express");
 
 const app = express();
 
+//Chapter -03
+
 // for incoming request
-//Here in namaste dev use 'use' keyword instead of get but when i use it is not showing proper response
-app.get("/", (req, res) => {
-  res.send("Hello from ther server!");
+// app.use("/dashboard", (req, res) => {
+//   res.send("This is Dashboard page!");
+// });
+// //This will match all the http method API calls to /test
+// app.use("/test", (req, res) => {
+//   res.send("This is Test page!");
+// });
+// app.use("/", (req, res) => {
+//   res.send("Hello from ther server!");
+// });
+
+// ===========================================================
+
+// Chapter - 04
+
+// if i add app.use in top then every method or api call will go only through it, never got chance get, post, put, delete
+// app.use("/user", (req, res) => res.send("HAHAHAHAHAHA!!"));
+
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Nitya", lastName: "Sahu" });
 });
 
-app.get("/dashboard", (req, res) => {
-  res.send("This is Dashboard page!");
+app.post("/user", (req, res) => {
+  //Saving data to DB
+  res.send("Data successfully saved to Database!");
 });
-app.get("/test", (req, res) => {
+
+app.delete("/user", (req, res) => {
+  res.send("Delete DAta!");
+});
+
+//This will match all the http method API calls to /test
+app.use("/test", (req, res) => {
   res.send("This is Test page!");
 });
 
